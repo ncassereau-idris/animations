3c6eff909ae234f78b756fd76d22688a08c36ab1
Nathan Cassereau <nathan.cassereau@idris.fr>
2023-02-20 17:22:02 +0100

    imports. Also added a scene file for scene generation based on terminal instead of jupyter notebook (does all the imports and makes sure manim finds all scenes)

---
+++ src/ddp/__init__.py
@@ -0,0 +1,9 @@
+from .ddp_scene import DDPScene
+from .zero_dp_stage1_scene import ZeroDPStage1Scene
+from .zero_dp_stage2_scene import ZeroDPStage2Scene
+
+__all__ = [
+    "DDPScene",
+    "ZeroDPStage1Scene",
+    "ZeroDPStage2Scene"
+]
